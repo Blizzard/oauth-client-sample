8e6c733c09eca4b34a3f5c1bde6b9737f6a4eaee
Tyler Gregory <tyler.w.gregory@gmail.com>
2021-06-16 23:14:34 -0700

    init for webflux

---
+++ java-webflux/src/main/java/com/blizzard/javawebfluxoauthsample/JavaWebfluxApplication.java
@@ -0,0 +1,13 @@
+package com.blizzard.javawebfluxoauthsample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaWebfluxApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaWebfluxApplication.class, args);
+	}
+
+}
